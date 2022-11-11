7f7f1fe6f86785399d350cda293037b69a9eead0
sttilstra <77122599+sttilstra@users.noreply.github.com>
2022-11-11 13:06:50 -0500

    Create detect_language.py

---
+++ detect_language.py
@@ -0,0 +1,50 @@
+import logging
+import requests
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from urllib.parse import parse_qs
+
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    key = "<key>"
+    endpoint = "<endpoint>"
+    location = "<location>"
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json'
+    }
+
+    req_body_bytes = req.get_body()
+    logging.info(req_body_bytes)
+
+    user_statement = req_body_bytes.decode("utf-8")
+    logging.info(user_statement)
+
+
+    # this section detects the language from the initial interaction
+    path_detect = '/detect'
+    url_detect = endpoint + path_detect
+
+    params_detect = {
+        'api-version': '3.0'
+    }
+
+    body_detect = [{
+        'text': user_statement
+    }]
+
+    request_detect = requests.post(url_detect, params=params_detect, headers=headers, json=body_detect)
+    request_detect.raise_for_status()
+    response_detect = request_detect.json()
+    detected_language = response_detect[0]["language"]
+
+    #print(response_detect)
+    #print(detected_language)
+
+    return func.HttpResponse(detected_language)
